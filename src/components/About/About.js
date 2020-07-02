19c8599989a48d5f3d21f9173c28e04b04348b6c
azakimi123 <aaron.zakimi@gmail.com>
2020-07-02 13:51:49 -0600

    about render prop adjusted

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import History from '../History/History';
 import Contact from '../Contact/Contact';
+import AboutText from '../About/AboutText';
 
 
 export default class About extends Component {
@@ -24,25 +25,8 @@ export default class About extends Component {
           <Switch>
             <Route path='/about/history' component={History}/>
             <Route path='/about/contact' component={Contact} />
-            <Route exact path='/about' render={ () => (
-              <div>
-              <h1 className="title">About WestSide University:</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-                eu lorem et ultricies. In porta lorem at dui semper porttitor. Nullam
-                quis cursus dui. Cras tincidunt vehicula tellus eu facilisis. Donec
-                nisi turpis, iaculis et arcu a, aliquet ultrices nisl. Nam in pharetra
-                odio, ac blandit metus. Suspendisse potenti. Praesent elementum diam
-                non orci cursus rutrum. Pellentesque condimentum ultrices dignissim.
-                Sed a tempor ligula, vel luctus sapien. Mauris vehicula rutrum massa.
-                Duis condimentum, ex quis ullamcorper rhoncus, erat libero tempor
-                arcu, condimentum facilisis tellus lectus ut nunc. Pellentesque vitae
-                faucibus diam. Vestibulum eu erat ex. Ut justo neque, varius aliquet
-                erat vel, scelerisque convallis lacus. Mauris semper lorem mauris, sed
-                dignissim eros consectetur nec.
-              </p>
-            </div>
-            )}/>
+            {/* https://dev.to/cesareferrari/using-the-route-render-prop-in-react-k5a */}
+            <Route exact path='/about' render={ () => <AboutText/>} />
           </Switch>
         </div>
       </div>
